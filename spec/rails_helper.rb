bb8b3eea7bdf88e7812864818de5f9adddfa5bda
zfletch <zfletch2@gmail.com>
2020-05-04 10:40:53 -0400

    update dependencies

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,6 +60,7 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include FactoryBot::Syntax::Methods
+  config.include ActiveJob::TestHelper
 end
 
 Shoulda::Matchers.configure do |config|
